8c96c9415ad84204ee34436324687c15aba8163e
Marc Inizan <locstaw@gmail.com>
2020-01-14 14:38:49 +0000

    added functions

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+import operator
 
 def print_corr(df, pct=0):
     
@@ -24,3 +27,31 @@ def print_corr(df, pct=0):
 
     # Draw the heatmap with the mask and correct aspect ratio
     sns.heatmap(corr, mask=mask, cmap=cmap, vmax=1, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+def p_dropping(df, target, p_value):
+
+    features = list(df.drop([target], axis=1).columns)
+    drop_from_x = [target]
+    # Fitting the actual model
+    y = df[target].astype('float')
+    X = sm.add_constant(df.drop([target], axis=1).astype('float'))
+    model = sm.OLS(y, X)
+    results = model.fit()
+    dict_p_values = results.pvalues.to_dict()
+    del dict_p_values["const"]
+    max_p_param = max(dict_p_values.items(), key=operator.itemgetter(1))[0]
+    max_p = max(dict_p_values.values())
+    best_model = results
+    while max_p > p_value:
+        #Fitting the new model
+        drop_from_x.append(max_p_param)
+        y = df[target].astype('float')
+        X = sm.add_constant(df.drop(drop_from_x, axis=1).astype('float'))
+        model = sm.OLS(y, X)
+        results = model.fit()
+        dict_p_values = results.pvalues.to_dict()
+        del dict_p_values["const"]
+        max_p_param = max(dict_p_values.items(), key=operator.itemgetter(1))[0]
+        max_p = max(dict_p_values.values())
+        best_model = results
+    return best_model, drop_from_x
